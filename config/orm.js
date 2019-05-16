4566f18b58bfabca35020fb66dfef1c057f6aaef
Affiliatethis <chris@onecrookedtree.com>
2019-05-16 10:35:33 -0400

    Working on adding the necessary code to the orm, burger_controller folders.  This is the 3rd update.

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var orm = require("../config/connection.js");
+var connection= require("../config/connection.js");
 
 // printing  question marks since we are doing 3 operations
 function printQuestionMarks(num) {
@@ -15,6 +15,26 @@ function printQuestionMarks(num) {
 
 // function to convert object to SQL Syntax
 function objToSql(ob) {
-    var arr= []
+    var arr= [];
+
+    // this is a loop to get through the keys and push the values as
+    // string
+for (var key in ob) {
+    var value = ob[key];
+
+    if (Object.hasOwnProperty.call(ob.key)) {
+        
+
+    }
+
+    
+
+
+
+}
+    
+
+
+
 
 }
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("goat cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("smokehouse burger", false);   
