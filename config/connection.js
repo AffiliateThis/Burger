99338557030f3e5e563f341c97d2c7617b9dae52
Affiliatethis <chris@onecrookedtree.com>
2019-05-17 10:58:04 -0400

    Heroku code added to connection.js file to connect to Heroku db.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Steak12",
-    database: "burger_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Steak12",
+        database: "burger_db"
+    });
+};
 
 connection.connect(function (err) {
     if (err) {
@@ -16,4 +20,5 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
+connection.connect();
 module.exports = connection;
