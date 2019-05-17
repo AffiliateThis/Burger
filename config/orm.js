a3d47193a1e9f33c54c01dccf6933e68c391a88c
Affiliatethis <chris@onecrookedtree.com>
2019-05-16 21:36:39 -0400

    4th update.   It is now working as the table is displaying in Node.  Will be doing a heroku upload soon as well.

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,9 @@
-var connection= require("../config/connection.js");
+var connection = require("../config/connection.js");
+
 
 // printing  question marks since we are doing 3 operations
 function printQuestionMarks(num) {
-    var arr= [];
+    var arr = [];
 
     for (var i = 0; i < num; i++) {
         arr.push("?");
@@ -15,26 +16,76 @@ function printQuestionMarks(num) {
 
 // function to convert object to SQL Syntax
 function objToSql(ob) {
-    var arr= [];
+    var arr = [];
 
     // this is a loop to get through the keys and push the values as
     // string
-for (var key in ob) {
-    var value = ob[key];
+    for (var key in ob) {
+        var value = ob[key];
 
-    if (Object.hasOwnProperty.call(ob.key)) {
-        
+        if (Object.hasOwnProperty.call(ob.key)) {
+            if (typeof value === "string" && value.indexOf("") >= 0) {
+                value = "'" + value + "'";
+
+            }
+            arr.push(key + "=" + value);
+        }
+
+
+
+
 
     }
 
-    
+    return arr.toString();
 
 
 
 }
-    
 
 
+
+var orm = {
+    all: function (tableInput, cb) {
+
+        connection.query("SELECT * FROM burgers", function (err, res) {
+            if (err) {
+                throw err;
+            }
+
+            cb(res)
+        });
+    },
+
+    insertBurger: function (burger_name, res) {
+        connection.query("INSERT INTO burgrs SET?", {
+            burger_name: burger_name,
+            devoured: false,
+
+        },
+            function (err, res) {
+                if (err) throw err;
+                return res;
+            });
+
+    },
+    updateBurger: function (oneBurger, res) {
+        connection.query("UPDATE burgers SET? WHERE?", [{
+            devoured: true
+        }, { id: oneBurger }], function (err, res) {
+            if (err) throw err;
+            return res;
+        });
+
+    }
 
 
 }
+
+
+
+
+
+// cb(res);
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8081;
 
 var app = express();
 
