444edee56e420120e3c434b14e02473c25490d2c
Affiliatethis <chris@onecrookedtree.com>
2019-05-18 09:52:33 -0400

    Fixed db connection

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,23 @@
 var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "Steak12",
-        database: "burger_db"
-    });
+	var connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "Steak12",
+		database: "burger_db"
+	});
 };
 
 connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+	if (err) {
+		console.error("error connecting: " + err.stack);
+		return;
+	}
+	console.log("connected as id " + connection.threadId);
 });
 
-connection.connect();
 module.exports = connection;
